154f2692de65dce2738e59df00b10371b0f96def
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-03-20 18:12:36 +0200

    added a toggle btn with its respective css file and added event listeners that allow us to click the manu icon and open the modal and go back to main page

--- shared.js
+++ shared.js
@@ -1,6 +1,9 @@
 const backdrop = document.querySelector('.backdrop')
 const modal = document.querySelector('.modal')
+const modalNoBtn = document.querySelector('.modal__action--negative')
 const selectPlanBtns = document.querySelectorAll('.Plan button')
+const toggleBtn = document.querySelector('.toggle-button')
+const mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanBtns.length; i++) {
     selectPlanBtns[i].addEventListener('click', () => {
@@ -8,3 +11,20 @@ for (var i = 0; i < selectPlanBtns.length; i++) {
         backdrop.style.display = 'block';
     })
 }
+
+backdrop.addEventListener('click', () => {
+    mobileNav.style.display = 'none';
+    closeModal();
+})
+
+modalNoBtn.addEventListener('click', closeModal)
+
+function closeModal() {
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+}
+
+toggleBtn.addEventListener('click', () => {
+    mobileNav.style.display = 'block';
+    backdrop.style.display = 'block';
+})
