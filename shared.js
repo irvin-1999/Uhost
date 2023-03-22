ff083518fef535364c3d8a729acfcdeb8d420855
Boitumelo_Iven Moshitwa <itumelo553@gmail.com>
2023-03-22 17:11:49 +0200

    added class method on the js file to manipulate the css

--- shared.js
+++ shared.js
@@ -7,24 +7,31 @@ const mobileNav = document.querySelector('.mobile-nav')
 
 for (var i = 0; i < selectPlanBtns.length; i++) {
     selectPlanBtns[i].addEventListener('click', () => {
-        modal.style.display = 'block';
-        backdrop.style.display = 'block';
+        // modal.style.display = 'block';
+        // backdrop.style.display = 'block';
+        modal.classList.add('open');
+        backdrop.classList.add('open')
     })
 }
 
 backdrop.addEventListener('click', () => {
-    mobileNav.style.display = 'none';
+    // mobileNav.style.display = 'none';
+    mobileNav.classList.remove('open');
     closeModal();
 })
 
 modalNoBtn.addEventListener('click', closeModal)
 
 function closeModal() {
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
+    // backdrop.style.display = 'none';
+    // modal.style.display = 'none';
+    backdrop.classList.remove('open')
+    modal.classList.remove('open');
 }
 
 toggleBtn.addEventListener('click', () => {
-    mobileNav.style.display = 'block';
-    backdrop.style.display = 'block';
+    // mobileNav.style.display = 'block';
+    // backdrop.style.display = 'block';
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open')
 })
